a93558a0753526dc9b5115d59d5517428b8a80d4
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-10-15 13:58:08 -0400

    Update PP2_2_test.py

--- PP2_2_test.py
+++ PP2_2_test.py
@@ -10,7 +10,7 @@ def test_q1_1(capsys):
 	except:
 		sys.exit()
 
-	input_values = ['knife']
+	input_values = ['10']
 
 	def mock_input(s):
 		print(s, end='')
@@ -19,7 +19,7 @@ def test_q1_1(capsys):
 
 	PP2_2.q1()
 	captured = capsys.readouterr()
-	assert captured.out == "In: -ives\n"
+	assert captured.out == "Input an integer: The number is not Five\n"
 
 def test_q2_1(capsys):
 
@@ -38,7 +38,45 @@ def test_q2_1(capsys):
 
 	PP2_2.q2()
 	captured = capsys.readouterr()
-	assert captured.out == "In: 10 is positive\n"
+	assert captured.out == "Input a number: Positive\n"
+
+def test_q3_1(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['10']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "Input an integer: Even\n"
+
+def test_q4_1(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['Hello']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q4()
+	captured = capsys.readouterr()
+	assert captured.out == 'Type "Hello": The word is Hello\n'
 
 def test_q1_2(capsys):
 
@@ -48,7 +86,7 @@ def test_q1_2(capsys):
 	except:
 		sys.exit()
 
-	input_values = ['hey']
+	input_values = ['5']
 
 	def mock_input(s):
 		print(s, end='')
@@ -57,9 +95,85 @@ def test_q1_2(capsys):
 
 	PP2_2.q1()
 	captured = capsys.readouterr()
-	assert captured.out == "In: -eys\n"
+	assert captured.out == "Input an integer: The number is Five\n"
 
 def test_q2_2(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['-5']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Negative\n"
+
+def test_q3_2(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['9']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "Input an integer: Odd\n"
+
+def test_q4_2(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['Hello!']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q4()
+	captured = capsys.readouterr()
+	assert captured.out == 'Type "Hello": The word is not Hello\n'
+
+def test_q1_3(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['-5']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "Input an integer: The number is not Five\n"
+
+def test_q2_3(capsys):
 
 	try:
 		exists = os.path.exists("PP2_2.py")
@@ -76,9 +190,9 @@ def test_q2_2(capsys):
 
 	PP2_2.q2()
 	captured = capsys.readouterr()
-	assert captured.out == "In:\n"
+	assert captured.out == "Input a number: Negative\n"
 
-def test_q1_3(capsys):
+def test_q3_3(capsys):
 
 	try:
 		exists = os.path.exists("PP2_2.py")
@@ -86,7 +200,45 @@ def test_q1_3(capsys):
 	except:
 		sys.exit()
 
-	input_values = ['helpp']
+	input_values = ['0']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "Input an integer: Even\n"
+
+def test_q4_3(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [' Hello ']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q4()
+	captured = capsys.readouterr()
+	assert captured.out == 'Type "Hello": The word is not Hello\n'
+
+def test_q1_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['0']
 
 	def mock_input(s):
 		print(s, end='')
@@ -95,9 +247,85 @@ def test_q1_3(capsys):
 
 	PP2_2.q1()
 	captured = capsys.readouterr()
-	assert captured.out == "In: -s\n"
+	assert captured.out == "Input an integer: The number is not Five\n"
 
-def test_q2_3(capsys):
+def test_q2_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['5.5']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Positive\n"
+
+def test_q3_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['-2']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "Input an integer: Even\n"
+
+def test_q4_4(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['hello']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q4()
+	captured = capsys.readouterr()
+	assert captured.out == 'Type "Hello": The word is not Hello\n'
+
+def test_q2_5(capsys):
+
+	try:
+		exists = os.path.exists("PP2_2.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = ['-12.7']
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP2_2.input = mock_input
+
+	PP2_2.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Negative\n"
+
+def test_q3_5(capsys):
 
 	try:
 		exists = os.path.exists("PP2_2.py")
@@ -112,11 +340,11 @@ def test_q2_3(capsys):
 		return input_values.pop(0)
 	PP2_2.input = mock_input
 
-	PP2_2.q2()
+	PP2_2.q3()
 	captured = capsys.readouterr()
-	assert captured.out == "In: -3 is negative\n"
+	assert captured.out == "Input an integer: Odd\n"
 
-def test_q1_4(capsys):
+def test_q4_5(capsys):
 
 	try:
 		exists = os.path.exists("PP2_2.py")
@@ -124,33 +352,13 @@ def test_q1_4(capsys):
 	except:
 		sys.exit()
 
-	input_values = ['Daisy']
+	input_values = ['Bye']
 
 	def mock_input(s):
 		print(s, end='')
 		return input_values.pop(0)
 	PP2_2.input = mock_input
 
-	PP2_2.q1()
+	PP2_2.q4()
 	captured = capsys.readouterr()
-	assert captured.out == "In: -ies\n"
-
-def test_q2_4(capsys):
-
-	try:
-		exists = os.path.exists("PP2_2.py")
-		assert exists
-	except:
-		sys.exit()
-
-	input_values = ['5']
-
-	def mock_input(s):
-		print(s, end='')
-		return input_values.pop(0)
-	PP2_2.input = mock_input
-
-	PP2_2.q2()
-	captured = capsys.readouterr()
-	assert captured.out == "In: 5 is positive\n"
-
+	assert captured.out == 'Type "Hello": The word is not Hello\n'
